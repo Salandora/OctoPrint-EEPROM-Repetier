5e93a4c0bb885c4efabc4fb8fc8faa7179f7ab91
wotsamatter <wotsamatter@gmail.com>
2021-03-13 17:04:47 +1100

    Backup feature for testing

--- octoprint_eeprom_repetier/__init__.py
+++ octoprint_eeprom_repetier/__init__.py
@@ -10,14 +10,34 @@ from __future__ import absolute_import, unicode_literals
 
 import octoprint.plugin
 import octoprint.server
+import flask
+from flask.json import jsonify
+
+import io
+import json
+import os
+import time
+
+__plugin_name__ = "EEPROM Editor - Repetier"
 
 __plugin_pythoncompat__ = ">=2.7,<4"
 
+BACKUP_FILE_PREFIX = "eeprom-backup"
+BACKUP_FILE_EXTENSION = ".repr"
+
+BACKUP_DATE_TIME_FMT = "%Y%m%d-%H%M%S"
+
+def build_backup_filename():
+    return "{}-{}{}".format(BACKUP_FILE_PREFIX, time.strftime(BACKUP_DATE_TIME_FMT), BACKUP_FILE_EXTENSION)
+
+
 class Eeprom_repetierPlugin(octoprint.plugin.AssetPlugin,
-                            octoprint.plugin.TemplatePlugin):
+                            octoprint.plugin.TemplatePlugin,
+                            octoprint.plugin.BlueprintPlugin):
     def get_assets(self):
         return dict(
-            js=["js/eeprom_repetier.js"]
+            js=["js/eeprom_repetier.js"],
+            css=["css/eeprom_repetier.css"]
         )
 
     def get_template_configs(self):
@@ -42,7 +62,111 @@ class Eeprom_repetierPlugin(octoprint.plugin.AssetPlugin,
             )
         )
 
-__plugin_name__ = "EEPROM Editor - Repetier"
+    ## Environment methods
+    def get_backup_folder(self):
+        return self.get_plugin_data_folder()
+
+    def get_full_path(self, filename):
+        return os.path.realpath(os.path.join(self.get_backup_folder(), filename))
+
+    def get_file_url(self, filename):
+        return flask.url_for("index") + "plugin/eeprom_repetier/download/" + filename
+
+    ## BlueprintPlugin
+    @octoprint.plugin.BlueprintPlugin.route("/list", methods=["GET"])
+    def get_list(self):
+        backup_folder = self.get_backup_folder()
+
+        dir_list = os.listdir(backup_folder)
+        backup_list = [{"filename":item, "url":self.get_file_url(item)} for item in dir_list if item.endswith(BACKUP_FILE_EXTENSION)]
+        response = flask.jsonify(
+            info = "Backup files", files = backup_list
+        )
+        response.status_code = 200
+        return response
+
+    @octoprint.plugin.BlueprintPlugin.route("/backup", methods=["POST"])
+    def create_backup(self):
+        filename = build_backup_filename()
+        full_path = self.get_full_path(filename)
+        backup_data = flask.request.json
+        response_status = 201
+
+        try:
+            file = open(full_path,"w")
+            file.write(json.dumps(backup_data, indent=2))
+            file.close()
+            self._logger.info("Created new EEPROM backup {}".format(filename))
+        except Exception:
+            self._logger.exception("Could not create EEPROM backup file {}".format(filename))
+            response_status = 500
+
+        response = flask.jsonify(name=filename)
+        response.status_code = response_status
+        return response
+
+    @octoprint.plugin.BlueprintPlugin.route("/backup/<filename>", methods=["GET"])
+    def get_backup(self, filename):
+        full_path = self.get_full_path(filename)
+        data = ""
+        response_status = 200
+
+        if (os.path.exists(full_path)):
+            try:
+                file = open(full_path,"r")
+                data = file.read()
+                file.close()
+                self._logger.info("Read EEPROM backup {}".format(filename))
+            except Exception:
+                self._logger.exception("Could not read {}".format(filename))
+                response_status = 404
+                #raise
+        else:
+            self._logger.warning("Requested backup file {} not found.".format(filename))
+            response_status = 204
+
+        response = flask.jsonify(name=filename, data=data)
+        response.status_code = response_status
+        return response
+
+    @octoprint.plugin.BlueprintPlugin.route("/backup/<filename>", methods=["DELETE"])
+    def delete_backup(self, filename):
+        backup_folder = self.get_backup_folder()
+        full_path = self.get_full_path(filename)
+        response_status = 200
+
+        if (os.path.exists(full_path)):
+            try:
+                os.remove(full_path)
+                self._logger.info("Deleted EEPROM backup {}".format(filename))
+            except Exception:
+                self._logger.exception("Could not delete {}".format(filename))
+                response_status = 404
+                #raise
+        else:
+            self._logger.warning("Backup file {} not found.".format(filename))
+
+        response = flask.jsonify(name=filename, data=[])
+        response.status_code = response_status
+        return response
+
+    ## tornado hooks for static file download
+
+    def route_hook(self, *args, **kwargs):
+        from octoprint.server import app
+        from octoprint.server.util.tornado import LargeResponseHandler
+
+        return [
+            (
+                r"/download/(.*)",
+                LargeResponseHandler,
+                {
+                    "path": self.get_plugin_data_folder(),
+                    "as_attachment": True
+                },
+            )
+        ]
+
 
 def __plugin_load__():
     global __plugin_implementation__
@@ -50,6 +174,7 @@ def __plugin_load__():
 
     global __plugin_hooks__
     __plugin_hooks__ = {
+        "octoprint.server.http.routes": __plugin_implementation__.route_hook,
         "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
     }
 
--- octoprint_eeprom_repetier/static/js/eeprom_repetier.js
+++ octoprint_eeprom_repetier/static/js/eeprom_repetier.js
@@ -10,8 +10,11 @@ $(function() {
 
         self.firmwareRegEx = /FIRMWARE_NAME:([^\s]+)/i;
         self.repetierRegEx = /Repetier_([^\s]*)/i;
+        self.firmwareVersion = "Unknown";
 
         self.eepromDataRegEx = /EPR:(\d+) (\d+) ([^\s]+) (.+)/;
+
+        self.pluginUrl = "plugin/eeprom_repetier/";
 
         self.isRepetierFirmware = ko.observable(false);
 
@@ -20,7 +23,11 @@ $(function() {
                    self.connection.isReady() || self.connection.isPaused();
         });
 
+        self.showOriginalValues = ko.observable(false);
+
         self.eepromData = ko.observableArray([]);
+
+        self.backupFiles = ko.observableArray([]);
 
         self.onStartup = function() {
             $('#settings_plugin_eeprom_repetier_link a').on('show', function(e) {
@@ -31,22 +38,25 @@ $(function() {
 
         self.fromHistoryData = function(data) {
             _.each(data.logs, function(line) {
-                var match = self.firmwareRegEx.exec(line);
-                if (match != null) {
-                    if (self.repetierRegEx.exec(match[0]))
-                        self.isRepetierFirmware(true);
-                }
+                self.checkRepetierFirmware(line);
+                // var match = self.firmwareRegEx.exec(line);
+                // if (match != null) {
+                //     if (self.repetierRegEx.exec(match[0]))
+                //         self.isRepetierFirmware(true);
+                // }
             });
         };
 
         self.fromCurrentData = function(data) {
             if (!self.isRepetierFirmware()) {
                 _.each(data.logs, function (line) {
-                    var match = self.firmwareRegEx.exec(line);
-                    if (match) {
-                        if (self.repetierRegEx.exec(match[0]))
-                            self.isRepetierFirmware(true);
-                    }
+                    self.checkRepetierFirmware(line);
+
+                    // var match = self.firmwareRegEx.exec(line);
+                    // if (match) {
+                    //     if (self.repetierRegEx.exec(match[0]))
+                    //         self.isRepetierFirmware(true);
+                    // }
                 });
             }
             else
@@ -72,9 +82,21 @@ $(function() {
             }
         };
 
+        self.checkRepetierFirmware = function(line) {
+            var firmware = "";
+            var match = self.firmwareRegEx.exec(line);
+            if (match) {
+                firmware = self.repetierRegEx.exec(match[0]);
+                if (firmware) {
+                    self.firmwareVersion = firmware[0];
+                    self.isRepetierFirmware(true);
+                }
+            }
+        };
+
         self.onEventConnected = function() {
             self._requestFirmwareInfo();
-        }
+        };
 
         self.onEventDisconnected = function() {
             self.isRepetierFirmware(false);
@@ -83,6 +105,8 @@ $(function() {
         self.loadEeprom = function() {
             self.eepromData([]);
             self._requestEepromData();
+            // Test
+            self.loadDummyData();
         };
 
         self.saveEeprom = function()  {
@@ -108,15 +132,98 @@ $(function() {
                     self.control.sendCustomCommand({ command: "M500"});
                 },
             });
-        }
+        };
+
+        self.listBackups = function () {
+
+            OctoPrint.get(self.pluginUrl+"list")
+            .done(function(response) {
+               self.backupFiles.removeAll();
+               response.files.sort().reverse();
+               _.each(response.files, function(f) {
+                   self.backupFiles.push({name: f.filename, url: f.url});
+               });
+            });
+            return true;
+        };
+
+        self.backupCurrent = function () {
+            
+            if (self.eepromData().length > 0) {
+                // Only include specific from the Eeprom data fields
+                var eepromData = self.eepromData();
+                var backupData = [];
+                _.each(eepromData, function(data) {
+                    backupData.push({
+                        position: data.position, 
+                        dataType: data.dataType, 
+                        value: data.value,
+                        description: data.description
+                    });
+                });
+                OctoPrint.postJson(
+                    self.pluginUrl+"backup",
+                    { backup_description:"Firmware version: "+self.firmwareVersion, backup_data: backupData }
+                )
+                .done(function(response) {
+                    self.showPopup("success", "Backup Complete.", "Current data backed up to " + response.name);
+                });
+                self.listBackups();
+            }
+        };
+
+        self.restoreBackup = function(filename) {
+            if (self.eepromData().length < 1) {
+                self.loadEeprom();
+            }
+
+            showConfirmationDialog({
+                message: "Replace the current display values with this backup data.",
+                onproceed: function() {
+                    OctoPrint.get(self.pluginUrl+"backup"+"/"+filename)
+                    .done(function(response) {
+                        var backupData = JSON.parse(response.data).backup_data;
+
+                        // Loop through backupData and replace the value of any matching elements in the current data set
+                        var eepromData = self.eepromData.removeAll();
+                        _.each(eepromData, function(e) {
+                            _.each(backupData, function(b) {
+                                if (b.position == e.position && b.dataType == e.dataType) {
+                                    e.value = b.value;
+                                }
+                            });
+                            self.eepromData.push(e);
+                        });
+
+                        self.showPopup("success", "Backup retrieved to current display values.", "Review the current values and load them to EEPROM if required.");
+                    });
+                    self.listBackups();
+                },
+            });
+        };
+
+        self.removeBackup = function(filename) {
+            showConfirmationDialog({
+                message: `Delete backup file ${filename}.`,
+                onproceed: function() {
+                    OctoPrint.delete(self.pluginUrl+"backup"+"/"+filename)
+                    .done(function(response) {
+                        self.showPopup("success", `Backup ${filename} deleted.`, "");
+                    });
+                    self.listBackups();
+                },
+            });
+        };
 
         self._requestFirmwareInfo = function() {
             self.control.sendCustomCommand({ command: "M115" });
         };
 
         self._requestEepromData = function() {
-            self.control.sendCustomCommand({ command: "M205" });
-        }
+            if (self.isRepetierFirmware()) {
+                self.control.sendCustomCommand({ command: "M205" });
+            }
+        };
 
         self._requestSaveDataToEeprom = function(data_type, position, value) {
             var cmd = "M206 T" + data_type + " P" + position;
@@ -128,33 +235,73 @@ $(function() {
                 cmd += " S" + value;
                 self.control.sendCustomCommand({ command: cmd });
             }
-        }
+        };
 
-        self.showPopup = function(message_type, title, text){
-            if (self.popup !== undefined){
-                    self.closePopup();
+        self.showPopup = function(message_type, title, text) {
+            if (self.popup !== undefined) {
+                self.closePopup();
             }
             self.popup = new PNotify({
-                    title: gettext(title),
-                    text: text,
-                    type: message_type,
-                    hide: false
+                title: gettext(title),
+                text: text,
+                type: message_type,
+                hide: false
             });
-        }
+        };
 
         self.closePopup = function() {
             if (self.popup !== undefined) {
                 self.popup.remove();
             }
         };
+
+        // ============= START TESTING CODE ==========================
+
+        // Inject dummy data into the EEPROM table for testing
+        self.loadDummyData = function () {
+            var dummyEepromData = [
+                {dataType: "S", position: 1, origValue: "45", value: "45", description: "A very long parameter description for Param 1"},
+                {dataType: "S", position: 2, origValue: "55", value: "25", description: "Param 2"},
+                {dataType: "S", position: 3, origValue: "65", value: "65", description: "Param 3"},
+                {dataType: "S", position: 4, origValue: "45", value: "45", description: "Param 4"},
+                {dataType: "S", position: 5, origValue: "55", value: "25", description: "Param 5"},
+                {dataType: "S", position: 6, origValue: "65", value: "65", description: "Param 6"},
+                {dataType: "S", position: 7, origValue: "45", value: "45", description: "Param 7"},
+                {dataType: "S", position: 8, origValue: "55", value: "25", description: "Param 8"},
+                {dataType: "S", position: 9, origValue: "65", value: "65", description: "Param 9"},
+                {dataType: "S", position:10, origValue: "65", value: "65", description: "Param 10"},
+                {dataType: "S", position:11, origValue: "45", value: "45", description: "Param 11"},
+                {dataType: "S", position:12, origValue: "55", value: "25", description: "Param 12"},
+                {dataType: "S", position:13, origValue: "65", value: "65", description: "Param 13"},
+                {dataType: "S", position:14, origValue: "45", value: "45", description: "Param 14"},
+                {dataType: "S", position:15, origValue: "55", value: "25", description: "Param 15"},
+                {dataType: "S", position:16, origValue: "65", value: "65", description: "Param 16"},
+                {dataType: "S", position:17, origValue: "45", value: "45", description: "Param 17"},
+                {dataType: "S", position:18, origValue: "55", value: "25", description: "Param 18"},
+                {dataType: "S", position:19, origValue: "65", value: "65", description: "Param 19"},
+                {dataType: "S", position:20, origValue: "65", value: "65", description: "Param 20"},
+                {dataType: "S", position:21, origValue: "45", value: "45", description: "Param 21"},
+                {dataType: "S", position:22, origValue: "55", value: "25", description: "Param 22"},
+                {dataType: "S", position:23, origValue: "65", value: "65", description: "Param 23"},
+                {dataType: "S", position:24, origValue: "45", value: "45", description: "Param 24"},
+                {dataType: "S", position:25, origValue: "55", value: "25", description: "Param 25"},
+                {dataType: "S", position:26, origValue: "65", value: "65", description: "Param 26"},
+                {dataType: "S", position:27, origValue: "45", value: "45", description: "Param 27"},
+                {dataType: "S", position:28, origValue: "55", value: "25", description: "Param 28"},
+                {dataType: "S", position:29, origValue: "65", value: "65", description: "Param 29"},
+                {dataType: "S", position:30, origValue: "65", value: "65", description: "Param 30"}
+            ];
+
+            _.each(dummyEepromData, function(e) {
+                self.eepromData.push(e);
+            });
+
+        };
+
+        // =============== END TESTING CODE ==========================
+
     }
-/* Old style viewmodel declaration - deprecated
-    OCTOPRINT_VIEWMODELS.push([
-        EepromRepetierViewModel,
-        ["controlViewModel", "connectionViewModel"],
-        "#settings_plugin_eeprom_repetier"
-    ]);
-*/
+
     OCTOPRINT_VIEWMODELS.push({
         construct: EepromRepetierViewModel,
         additionalNames: [],
